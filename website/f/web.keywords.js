469e5a6e89a02ba4de9716782ff3441749e5cd07
Unknown <unknown@example.com>
2020-11-19 22:45:46 +0300

    my new site

---
+++ website/f/web.keywords.js
@@ -0,0 +1,5 @@
+
+        window.$mediavine = window.$mediavine || {};
+        window.$mediavine.web = window.$mediavine.web || {};
+        window.$mediavine.web.keywords = {"status":"PROCESSED","sales_kw":"high-end fitness trackers,popular fitness trackers,trackers,expensive fitness trackers,easiest fitness trackers,best cheap fitness trackers,affordable fitness trackers,wearable fitness trackers,expensive trackers,cheap fitness trackers,affordable device,cheap,affordable,important fitness data,moov","sales_cat":"596,602,632,615"};
+      
