9be648f32836a0824476eea8d6c1958ebd24ab67
loketi <62499327+loketi@users.noreply.github.com>
2024-07-24 20:45:36 +0300

    Updates

--- website/pay-per-click/redir/bootstrapcreative.js
+++ website/pay-per-click/redir/bootstrapcreative.js
@@ -2,9 +2,9 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 var htmlString = `<div style="margin:60px auto;text-align:center;">
 <hr>
-<a href="" title="SEO & PPC Management Services in Nairobi"><img src="https://getfitech.com/img/logo.png" alt="Getfitech Consulting Logo" width="350" /></a>
+<a href="" title="Get Qualified B2B Leads to Fuel Your Sales Pipeline"><img src="https://getfitech.co.ke/img/getfitech-logo2.png" alt="Getfitech Media Logo" width="100" /></a>
 <br><br>
-<!--<a href="https://getfitech.com" target="_blank">Explore PPC Case Studies</a>&emsp;&emsp;&emsp;&emsp;<a href="https://getfitech.com/store/storefront" target="_blank">Explore Our Advertising Solutions</a>&emsp;&emsp;&emsp;&emsp;<a href="https://getfitech.com/store/faq.html" target="_blank"><strong>What is PPC?</strong></a>-->
+<!--<a href="https://getfitech.co.ke" target="_blank">Explore B2B Lead Generation Case Studies</a>&emsp;&emsp;&emsp;&emsp;<a href="https://getfitech.co.ke" target="_blank">Explore Our B2B Lead Generation Solution</a>&emsp;&emsp;&emsp;&emsp;<a href="https://getfitech.co.ke" target="_blank"><strong>What is B2B Lead Generation?</strong></a>-->
 </div>`;
 
 // BootstrapCreative 2018
